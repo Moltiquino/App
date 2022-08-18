dd9c2ea493c106a078699abd8e6ca00c69a9c496
Moltiquino <meghuaithal@gmail.com>
2022-08-18 14:23:24 +0530

    aeawe

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,3 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Sample App';
 
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
+asdsdad
